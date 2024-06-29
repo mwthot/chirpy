4f5d57339ceb1a76a410a3f4b8df283b9b5acd94
mwthot <mw.thot@gmail.com>
2024-06-29 15:27:23 -0700

    passed lesson 4

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mwthot/chirpy
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(s.ListenAndServe())
+}
